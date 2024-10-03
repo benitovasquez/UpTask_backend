2e3a4b7d7f0eaf42a049685ead50b3824fbfd9e9
benitovasquez <ricardobenitovasquezroca@gmail.com>
2024-10-03 10:28:51 -0400

    prueba de cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,17 +1,6 @@
-import {CorsOptions} from 'cors'
+import cors from 'cors';
 
-export const corsConfig: CorsOptions = {
-  origin: function(origin, callback) {
-    const whitelist = [process.env.FRONTEND_URL];
-    
-    if (process.argv[2] === '--api') {
-      whitelist.push(undefined);
-    }
-    
-    if (whitelist.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Error de CORS'));
-    }
-  }
+export const corsConfig = {
+  origin: process.env.FRONTEND_URL, // URL de tu frontend en producción
+  optionsSuccessStatus: 200
 };
