6f82e0431380077420d925ba1f9117155e5c8d1d
benitovasquez <ricardobenitovasquezroca@gmail.com>
2024-10-03 10:49:57 -0400

    Corrigiendo el deploy2

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,6 +1,17 @@
-import cors from 'cors';
+import { CorsOptions } from 'cors'
 
-export const corsConfig = {
-  origin: process.env.FRONTEND_URL, // URL de tu frontend en producción
-  optionsSuccessStatus: 200
-};
+export const corsConfig: CorsOptions = {
+    origin: function(origin, callback) {
+        const whitelist = [process.env.FRONTEND_URL]
+
+        if(process.argv[2] === '--api') {
+            whitelist.push(undefined)
+        }
+
+        if(whitelist.includes(origin)) {
+            callback(null, true)
+        } else {
+            callback(new Error('Error de CORS'))
+        }
+    }
+}
