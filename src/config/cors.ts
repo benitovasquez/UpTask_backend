f220eada3d0836a26caf0d1c2aaeabf45fcd54a4
benitovasquez <ricardobenitovasquezroca@gmail.com>
2024-10-04 07:40:25 -0400

    Corrigiendo cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,8 +2,19 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const allowedOrigins = [
+    process.env.FRONTEND_URL,            // Producción
+    'http://localhost:5173'              // Desarrollo
+];
+
 export const corsConfig = {
-  origin: process.env.FRONTEND_URL || "*", 
-  credentials: true,
-  optionsSuccessStatus: 200,
+    origin: function (origin, callback) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('No permitido por CORS'));
+        }
+    },
+    credentials: true,
+    optionsSuccessStatus: 200,
 };
